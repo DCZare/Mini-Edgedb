e87af88bafe4422e81091657d76d92e8243a4d8e
DCZare <88459946+DCZare@users.noreply.github.com>
2024-09-27 15:29:24 -0400

    Improvements

--- build-lite/src/app/page.tsx
+++ build-lite/src/app/page.tsx
@@ -22,27 +22,33 @@ const HomePage = async () => {
         <title>Works Dashboard</title>
         <meta name="description" content="List of works" />
       </Head>
-      <h1 style={{ textAlign: 'center' }}>Works</h1>
+      <h1 style={{ textAlign: 'center', fontSize: '2.5rem', margin: '20px 0' }}>
+        CSV Manager
+      </h1>
       {works.length === 0 ? (
         <p>No works found.</p>
       ) : (
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-          <div style={{ flex: '1', marginRight: '20px' }}>
-            <h2 style={{ textAlign: 'center' }}>Title</h2>
+        <div style={{ display: 'flex', justifyContent: 'center', width: '80%', margin: '0 auto' }}>
+          <div style={{ flex: 1, marginRight: '20px' }}>
+            <h2 style={{ margin: '0' }}>Title</h2>
             <ul style={{ listStyleType: 'none', padding: 0 }}>
               {works.map((work) => (
-                <li key={work.id} style={{ marginBottom: '10px' }}>
-                  {work.title}
+                <li key={work.id} style={{ marginBottom: '10px', height: '40px', display: 'flex', alignItems: 'center' }}>
+                  <div style={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
+                    {work.title}
+                  </div>
                 </li>
               ))}
             </ul>
           </div>
-          <div style={{ flex: '1' }}>
-            <h2 style={{ textAlign: 'center' }}>DOI</h2>
+          <div style={{ flex: 1 }}>
+            <h2 style={{ margin: '0' }}>DOI</h2>
             <ul style={{ listStyleType: 'none', padding: 0 }}>
               {works.map((work) => (
-                <li key={work.id} style={{ marginBottom: '10px' }}>
-                  {work.doi}
+                <li key={work.id} style={{ marginBottom: '10px', height: '40px', display: 'flex', alignItems: 'center' }}>
+                  <div style={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
+                    {work.doi}
+                  </div>
                 </li>
               ))}
             </ul>
